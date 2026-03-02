2b4cd13b0fb2baee0983d0c76413c1b84d76d9a7
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-03-01 19:18:30 -0800

    update+done

--- Color.cpp
+++ Color.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+const int W10 = 10;
 
 class Color{
 private:
@@ -15,15 +18,31 @@ public:
 
     //member print function
     void print(){
-        cout << "Red Value: " << red << endl;
-        cout << "Green Value: " << green << endl;
-        cout << "Blue Value: " << blue << endl;
+        cout << setw(W10) << "Red Value: " << red << endl;
+        cout << setw(W10) << "Green Value: " << green << endl;
+        cout << setw(W10) << "Blue Value: " << blue << endl;
     }
 };
 
 
 
 int main(){
+    //creating Color objects and populating
+    Color color1;
+    Color color2;
 
+    color1.setRed(23);
+    color1.setGreen(324);
+    color1.setBlue(67);
+
+    color2.setRed(873);
+    color2.setGreen(677);
+    color2.setBlue(767);
+
+    //outputting
+    cout << "Color 1:\n";
+    color1.print();
+    cout << "\nColor 2:\n";
+    color2.print();
     return 0;
 }
